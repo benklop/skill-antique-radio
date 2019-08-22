9b1e6442c0aa01c5f4799633030d1faee5eb4005
Ben Klopfenstein <benklop@gmail.com>
2019-08-22 16:13:18 -0600

    try threading

--- audio_vis.py
+++ audio_vis.py
@@ -8,6 +8,8 @@ from numpy import int16, empty, fromstring, roll
 from gu7000 import GU7000Ser as display
 from PIL import Image, ImageDraw
 
+import threading
+
 WIDTH=140
 HEIGHT = 16
 
@@ -18,27 +20,9 @@ FFT_LEN = CHUNK*20 # size of rolling buffer for FFT
 RATE = 8000 # Sampling rate
 SIGNAL_SCALE = .005 # Scaling factor for output
 
-SPARKS = [
-  ' ',
-  '\u2581',
-  '\u2582',
-  '\u2583',
-  '\u2584',
-  '\u2585',
-  '\u2586',
-  '\u2587',
-  '\u2588'
-]
-SPARKS_LEN = len(SPARKS)
+im = Image.new('1', (WIDTH, HEIGHT), 255)
 
-
-def spark(i, full):
-    i = min(int(max(0.0, i) * SPARKS_LEN), SPARKS_LEN-1)
-    if full > HEIGHT:
-        return '\033[0;31m' + SPARKS[i] + '\033[0m'
-    return SPARKS[i]
-
-def run():
+def gather_audio():
     p = pyaudio.PyAudio()
     stream = p.open(
         format=pyaudio.paInt16,
@@ -51,9 +35,6 @@ def run():
 
     d = display(140, 16, dev='/dev/ttyS2')
 
-    this_frame = 0
-    # Disable cursor
-    sys.stdout.write('\033[?25l')
     while 1:
         # Roll in new frame into buffer
         try:
@@ -67,19 +48,21 @@ def run():
 
         # Now transform!
         try:
-            fftspec = list(log(abs(x) * SIGNAL_SCALE) + 8 for x in rfft(signal)[:WIDTH])
+            fftspec = list(log(abs(x) * SIGNAL_SCALE) + 2 for x in rfft(signal)[:WIDTH])
         except ValueError:
             fftspec = [0] * SLICES
 
-        this_frame = this_frame + 1
         #create an image
-        im = Image.new('1', (WIDTH, HEIGHT), 255)
-        draw = ImageDraw.Draw(im)
+        img = Image.new('1', (WIDTH, HEIGHT), 255)
+        draw = ImageDraw.Draw(img)
         points = [val for pair in zip(range(WIDTH), fftspec) for val in pair]
         draw.line(points, 0)
-        im = im.transpose(Image.FLIP_TOP_BOTTOM)
-        if this_frame % 20 == 0:
-            d.displayImage(im)
+        im = img.transpose(Image.FLIP_TOP_BOTTOM)
+
+def display_waveform():
+    while 1:
+        d.displayImage(im)
 
 if __name__ == "__main__":
-    run()
+     process = threading.Thread(target=gather_audio)
+     disp = threading.Thread(target=display_waveform)
