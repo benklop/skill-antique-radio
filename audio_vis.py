43b6b396b71f2f7f2c4f26bcd55f29c531c6a127
Ben Klopfenstein <benklop@gmail.com>
2019-08-22 13:15:49 -0600

    fixes

--- audio_vis.py
+++ audio_vis.py
@@ -5,7 +5,7 @@ import pyaudio
 from math import log
 from numpy.fft import rfft
 from numpy import int16, empty, fromstring, roll
-from gu7000 import GU7000Ser as display
+#from gu7000 import GU7000Ser as display
 from PIL import Image, ImageDraw
 
 WIDTH=140
@@ -16,7 +16,7 @@ SLICES=4
 CHUNK = int(WIDTH / SLICES) # Size of each 'frame' in rolling buffer
 FFT_LEN = CHUNK*20 # size of rolling buffer for FFT
 RATE = 8000 # Sampling rate
-SIGNAL_SCALE = .0005 # Scaling factor for output
+SIGNAL_SCALE = .00005 # Scaling factor for output
 
 SPARKS = [
   ' ',
@@ -49,7 +49,7 @@ def run():
     )
     signal = empty(FFT_LEN, dtype=int16)
 
-    d = display(140, 16, dev='/dev/ttyS2')
+    #d = display(140, 16, dev='/dev/ttyS2')
 
     try:
         # Disable cursor
@@ -67,25 +67,26 @@ def run():
 
             # Now transform!
             try:
-                fftspec = list(log(abs(x) * SIGNAL_SCALE) + 2 for x in rfft(signal)[:WIDTH])
+                fftspec = list(log(abs(x) * SIGNAL_SCALE) + 1 for x in rfft(signal)[:WIDTH])
             except ValueError:
                 fftspec = [0] * SLICES
 
             #create an image
             im = Image.new('1', (WIDTH, HEIGHT), 1)
             draw = ImageDraw.Draw(im)
-            draw.line(zip(iter(fftspec), iter(range(WIDTH))), 0)
-            d.displayImage(im)
+            points = [val for pair in zip(fftspec, range(WIDTH)) for val in pair]
+            draw.line(points, 0)
+            #d.displayImage(im)
 
             # Print it
             lines = [
                 ''.join(spark(x - i+1, x) for x in fftspec)
-                for i in range(HEIGHT / 8, 0, -1)
+                for i in range(int(HEIGHT / 8), 0, -1)
             ]
             sys.stdout.write('│' + '│\n│'.join(lines) + '│')
-            sys.stdout.write('\033[' + str((HEIGHT / 8) - 1) +'A\r')
+            sys.stdout.write('\033[' + str(int(HEIGHT / 8) - 1) +'A\r')
     except KeyboardInterrupt:
-        sys.stdout.write('\n' * HEIGHT / 8)
+        sys.stdout.write('\n' * int(HEIGHT / 8))
     finally:
         # Turn the cursor back on
         sys.stdout.write('\033[?25h')
